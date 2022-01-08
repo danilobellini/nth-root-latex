055dd9d8cfb1e2d651bf0df823f3096bfa5c2f3b
Danilo J. S. Bellini <danilo.bellini@gmail.com>
2022-01-08 01:09:41 -0300

    Replace initial condition a0 by (x - 1) // n + 1
    For integer x >= 0 and n > 0, that initial condition always works
    because it's the result of a manual iteration of the algorithm using 1
    as its input and ignoring the stop condition, but it's still not
    explained in the text

--- nth_root.py
+++ nth_root.py
@@ -1,5 +1,5 @@
-def nth_root(x, n, a0=None):
-    a = x if a0 is None else a0
+def nth_root(x, n):
+    a = (x - 1) // n + 1
     while a > (d := x // a ** (n - 1)):
         a = ((n - 1) * a + d) // n
     return a
